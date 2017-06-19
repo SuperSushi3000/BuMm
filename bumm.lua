7481d1988182b77a5ebf90855cff6a82652a3f09
SuperSushi3000 <b.roll@hellesdunkel.de>
2017-06-19 23:39:57 +0200

    changed gui website from inline html code to external file

--- bumm.lua
+++ bumm.lua
@@ -18,10 +18,16 @@ pwm.start(7)
 pin1_value = 0
 rgb_value = "#FFFFFF"
 
+file.open("/FLASH/BuMmControl.html")
+file_str = file.read()
+file.close()
+
 server = net.createServer(net.TCP)
 
 function on_receive(socket, data)
     print(data)
+    --remove lattenzaun because it begins with %(escape symbol)
+    data = string.gsub(data, "%%23", "")
     --parse for header with pattern GET /?key=value HTTP
     local _, _, method, path, vars = string.find(data, "([A-Z]+) (.+)?(.+) HTTP")
     if(method == nil) then
@@ -42,46 +48,33 @@ function on_receive(socket, data)
     
     if(values.pin0 == "on")then
         gpio.write(0, gpio.HIGH)
-        pin0_checked = "checked=\"checked\""--nice to read, isn't it?
+        pin0_checked = " checked=\"checked\""--nice to read, isn't it?
     elseif(values_changed) then
         gpio.write(0, gpio.LOW)
     end
 
   
     if(values.pin1)then
-        print(values.pin1)
         pin1_value = values.pin1
         pwm.setduty(1, pin1_value)
     end
 
     if(values.rgb)then
-        print("pommes")
-        print(values.rgb)
-        rgb_value = values.rgb
-        --TODO: set PWM channels
+        rgb_value = "#"..values.rgb
+        local color = tonumber(values.rgb, 16)
+        --set red pwm duty cycle
+        print(1023-bit.lshift(bit.band(color, 0xFF0000),2))
+        --pwm.setduty(6, )
+        --set green pwm duty cycle
+        --pwm.setduty(5, 1023-bit.lshift(bit.band(color, 0x00FF00),2))
+        --set blue pwm duty cycle
+        --pwm.setduty(7, 1023-bit.lshift(bit.band(color, 0x0000FF),2))
     end
 
-    local buffer = ""
-    buffer = [[
-        <h1>BuMm-Control</h1>
-        <form>
-            <h2>BuMmS</h2>
-            (Bjoern und Maltes multifunktionale Steckdose)<br>
-            <label>
-                <input type="checkbox" name="pin0"]]..pin0_checked..[[ onchange="form.submit()">
-                Pin 0
-            </label>
-            <h2>BuMmeL</h2>
-            (Bjoern und Maltes multifunktionale, elektronische Lampe)<br>
-            <label>
-                <input type="range" name="pin1" min="0" max="1023" value="]]..pin1_value..[[" onchange="form.submit()">
-                Pin 1
-            </label>
-            <label>
-                <input type="color" name="rgb" pattern="^#([A-Fa-f0-9]{6})$" required value="]]..rgb_value..[[" onchange="form.submit()">
-                Pin 5, 6, 7
-        </form>
-    ]]
+    local buffer = file_str
+    buffer = string.gsub(buffer, "<!%-%-VAR/pin0_checked%-%->", pin0_checked)
+    buffer = string.gsub(buffer, "<!%-%-VAR/pin1_value%-%->", pin1_value)
+    buffer = string.gsub(buffer, "<!%-%-VAR/rgb_value%-%->", rgb_value)
     socket:send(buffer)
 end
 
